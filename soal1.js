32a8cae856f7f12fa34c14bd1e1d2cdee0d7cb17
bagasandi270 <bagas.andianto270@gmail.com>
2023-06-18 22:41:44 +0700

    Update soal1.js

--- soal1.js
+++ soal1.js
@@ -1,18 +1,18 @@
 const prompt = require('prompt-sync')({ sigint: true });
 
 
-var angka = prompt("masukkan angka = ");
+var angka = prompt("Masukkan Angka = ");
 
-// cek, jika bukan angka && angka >= 0 (tidak minus)
+// cek jika bukan angka && angka >= 0 (tidak minus)
 if (!isNaN(angka) && angka >= 0) {
-  // cek inputan bilangan genap, jika angka di modulus 2 == 0
+  // cek apakah input bilangan genap, jika angka di modulus 2 == 0
   if (angka % 2 === 0) {
-    console.log("Angka yang anda input adalah bilangan genap"); // maka genap
+    console.log("Angka yang anda input adalah bilangan genap"); // adalah genap
     var hasil_akar = Math.sqrt(angka);
     console.log("Hasil akar kuadrat dari " + angka + " adalah " + hasil_akar);
   } else {
-    console.log("Tidak bisa input bilangan ganjil"); // maka ganjil
+    console.log("Tidak bisa input bilangan ganjil"); // adalah ganjil
   }
-} else { // jika inputan salah
+} else { // jika hasil input salah
   console.log("Input salah atau tidak bisa input bilangan negatif.");
 }
